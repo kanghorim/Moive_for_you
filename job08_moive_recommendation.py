6e5424d7f551247e2bff49ef027102736a65bf26
kanghorim <horim159456@naver.com>
2022-04-08 18:13:28 +0900

    이제 3분의1.5 진행

--- job08_moive_recommendation.py
+++ job08_moive_recommendation.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.metrics.pairwise import linear_kernel
 from scipy.io import mmread, mmwrite
 import pickle
+from gensim.models import Word2Vec
 
 df_reviews = pd.read_csv('./crawling_data/datasets/movie_review_2018_2022.csv')
 df_reviews.info()
@@ -24,8 +25,27 @@ with open('./models/tfidf.pickle', 'rb') as f:
 # movie_idx = df_reviews[df_reviews['title']=='기생충'].index[0]
 # print(movie_idx)
 # 영화 index를 이용
-movie_idx = 218
-print(df_reviews.iloc[movie_idx, 0])
-cosine_sim = linear_kernel(Tfidf_matrix[movie_idx], Tfidf_matrix)
+# movie_idx = 218
+# print(df_reviews.iloc[movie_idx, 0])
+
+embedding_model = Word2Vec.load('./models/word2vecModel.model')
+key_word = '동성애'
+sim_word = embedding_model.wv.most_similar(key_word, topn=10)
+sentence = [key_word] * 11
+
+words = []
+
+for word, _ in sim_word:
+    words.append(word)
+
+for i, word in enumerate(words):
+    sentence += [word] * (10 - 1)
+
+sentence = ' '.join(sentence)
+sentence_vec = Tfidf.transform([sentence])
+cosine_sim = linear_kernel(sentence_vec, Tfidf_matrix)
+
+
+#cosine_sim = linear_kernel(Tfidf_matrix[movie_idx], Tfidf_matrix)
 recommendation = getRecommendation(cosine_sim)
 print(recommendation)
